119f0e970c846aeb9183d8805795b3f899273300
Pixeleer <pixeleertheman@gmail.com>
2022-08-06 19:33:10 -0500

    Added comments

--- microscript.py
+++ microscript.py
@@ -22,24 +22,27 @@ def run_script(f_f):
             
             for line in f.readlines():
                 done = False
-
+                
+                line = line.split("//")[0]
                 args = line.split(" ")
                 cmd = args[0]
+                if cmd == "\n" or "" or " ":
+                    done = True
+                if cmd.startswith("//"):
+                    done = True
+                if cmd == "LOOP":
+                    _global.loop = True
+                    done = True
                 for module in os.listdir(r"ms_lib"):
                     module = module.upper().replace(".PY", "")
                     if module == '__init__.py':
                         continue
-                    if cmd == "\n":
-                        done = True
-                    if cmd == "LOOP":
-                        _global.loop = True
-                        done = True
                     if module == cmd:
                         exec(f"import ms_lib.{module}")
                         exec(f"ms_lib.{module}.lib(args)")
                         done = True
                 if done != True:
-                    report_error(f"[Microscript] {cmd} | Not found.", f_f)
+                    report_error(f"[microscript] {cmd} | Not found.", f_f)
             
         run()
 
@@ -62,8 +65,8 @@ if __name__ == "__main__":
                 for line in open("req.txt", 'r'):
                     os.system(f"{sys.executable} -m pip install {line}")
                 exit()
-            run_script(scr+".micro")
+            run_script(scr)
         else:
             run_script(settings[1])
     else:
-        report_error(f"[Microscript] \"run\" config variable not set.", "ms.conf")
+        report_error(f"[microscript] \"run\" config variable not set.", "ms.conf")
